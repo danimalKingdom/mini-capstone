7e380a349065243e7861a837dad608d625f950b1
dani <dani.zaghian@gmail.com>
2017-02-07 17:18:04 -0800

    multiplied order subtotal by quantity update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,9 @@ class OrdersController < ApplicationController
     carted_products = current_user.carted_products.where(status: "carted")
 
     order = Order.create(user_id: current_user.id)
+
     carted_products.update_all(status: "purchased", order_id: order.id)
+    
     order.calculate_totals
 
     flash[:success] = "Order successfully created"
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,15 +3,14 @@ class Order < ActiveRecord::Base
   has_many :carted_products
   has_many :products, through: :carted_products
 
-  tax_rate = 0.09
-
   def calculate_totals
     subtotal = 0
-    carted_products.each do |carted_product|
-      subtotal += carted_product.subtotal
-    end
 
-    tax = subtotal * tax_rate
+    carted_products.each do |carted_product|
+      subtotal += carted_product.product.price * carted_product.quantity
+    end 
+
+    tax = subtotal * 0.09
     total = subtotal + tax
     update(subtotal: subtotal, tax: tax, total: total)
   end
